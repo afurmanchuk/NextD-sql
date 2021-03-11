cccee488fbc243cb7585becc92ce79d15e01a4eb
afurmanchuk <furmanchuk@icnanotox.org>
2021-03-11 12:10:20 -0600

    Update NextD_ExtractionCode_PROCEDURES_CAPRICORN_2019_01_08.sql

--- NextD_ExtractionCode_PROCEDURES_CAPRICORN_2019_01_08.sql
+++ NextD_ExtractionCode_PROCEDURES_CAPRICORN_2019_01_08.sql
@@ -1,65 +1,42 @@
 ---------------------------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------------------
------                             Part 8: Procedures for the study sample                                 -----  
+-----                             Part 7: Procedures for the study sample                                 -----  
 --------------------------------------------------------------------------------------------------------------- 
 ---------------------------------------------------------------------------------------------------------------
 /* Tables for this eaxtraction: 
-1. Table 1 (named here #Final_Table1) with Next-D study sample IDs. See separate SQL code for producing this table.
-2. PROCEDURES and DEMOGRAPHIC table from PCORNET.
-3. Tabel with mapping (named here #GlobalIDtable) between CAPRICORN IDs and Global patient IDs provided by MRAIA. */
+1. Table 1 (named here #FinalTable1) with Next-D study sample IDs. See separate SQL code for producing this table.
+2. PROCEDURES and DEMOGRAPHIC table from PCORNET. */
+---------------------------------------------------------------------------------------------------------------
+use /*provide here the name of PCORI database here: */PCORI_SAS;
 ---------------------------------------------------------------------------------------------------------------
 -----                            Declare study time frame variables:                                      -----
-use capricorn;/*Specify PCORI database name here*/
-DECLARE @studyTimeRestriction int;declare @UpperTimeFrame DATE; declare @LowerTimeFrame DATE;
+DECLARE @studyTimeRestriction int;declare @UpperTimeFrame int; declare @LowerTimeFrame int;
 -----                             Specify time frame and age limits                                       -----
 --Set extraction time frame below. If time frames not set, the code will use the whole time frame available from the database
-set @LowerTimeFrame='2010-01-01';
-set @UpperTimeFrame='2019-10-31 23:59:59';/* insert here thw end exctraction date from iRB*/
+set @LowerTimeFrame=18263;--'2010-01-01';
+set @UpperTimeFrame=22280;--'2020-12-31';
 --set age restrictions:
 declare @UpperAge int; declare @LowerAge int;set @UpperAge=89; set @LowerAge=18;
 ---------------------------------------------------------------------------------------------------------------
-select c.PATID,
-		b.ENCOUNTERID,
-		b.PROCEDURESID,
-		b.ENC_TYPE,
-		year(b.ADMIT_DATE) as ADMIT_DATE_YEAR,
-		month(b.ADMIT_DATE) as ADMIT_DATE_MONTH,
-		datediff(dd,c.FirstVisit,b.ADMIT_DATE) as ADMIT_DATE_DAYSFROMFIRSTVISIT,
-		b.PROVIDERID,
-		year(b.PX_DATE) as PX_DATE_YEAR,
-		month(b.PX_DATE) as PX_DATE_MONTH,
-		datediff(dd,c.FirstVisit,b.PX_DATE) as PX_DATE_DAYSFROMFIRSTVISIT,
-		b.PX,b.PX_TYPE ,
-		b.PX_SOURCE,
-		b.PPX,
-		b.PROVIDERID
-	  into #NextD_PROCEDURES
-	  from /* provide name of table 1 here: */ #Final_Table1 c 
-	  left join [dbo].[PROCEDURES] b on c.PATID=b.PATID 
-	  left join dbo.DEMOGRAPHIC d on c.PATID=d.PATID
-	  where (datediff(yy,d.BIRTH_DATE,b.ADMIT_DATE) between @LowerAge and @UpperAge) and (b.ADMIT_DATE between @LowerTimeFrame and @UpperTimeFrame);
----------------------------------------------------------------------------------------------------------------
-select d.GLOBALID,
-		i.ENCOUNTERID,
-		i.PROCEDURESID,
-		i.ENC_TYPE,
-		i.ADMIT_DATE_YEAR,
-		i.ADMIT_DATE_MONTH,
-		i.ADMIT_DATE_DAYSFROMFIRSTVISIT,
-		i.PX_DATE_YEAR,
-		i.PX_DATE_MONTH,
-		i.PX_DATE_DAYSFROMFIRSTVISIT,
-		i.PX,
-		i.PX_TYPE,
-		i.PX_SOURCE,
-		i.PPX,
-		i.PROVIDERID
-into #NextD_PROCEDURES_FINAL
-from #NextD_PROCEDURES i 
-join  /* provide name the table containing c.PATID,Hashes, and Global patient id*/ #GlobalIDtable d 
-on i.PATID=c.PATID;
----------------------------------------------------------------------------------------------------------------
-/* Save #NextD_PROCEDURES_FINAL as csv file. 
-Use "|" symbol as field terminator and 
-"ENDALONAEND" as row terminator. */ 
+select c.PATID,'|' as Pipe1,
+	b.ENCOUNTERID,'|' as Pipe2,
+	b.PROCEDURESID,'|' as Pipe3,
+	b.ENC_TYPE,'|' as Pipe4,
+	year(dateadd(dd,b.ADMIT_DATE,'1960-01-01')) as ADMIT_DATE_YEAR,'|' as Pipe5,
+	month(dateadd(dd,b.ADMIT_DATE,'1960-01-01')) as ADMIT_DATE_MONTH,'|' as Pipe6,
+	b.ADMIT_DATE - c.FirstVisit as DAYS_from_FirstEncounter_Date1,'|' as Pipe7,
+	b.PROVIDERID,'|' as Pipe8,
+	year(dateadd(dd,b.PX_DATE,'1960-01-01')) as PX_DATE_YEAR,'|' as Pipe9,
+	month(dateadd(dd,b.PX_DATE,'1960-01-01')) as PX_DATE_MONTH,'|' as Pipe10,
+	b.PX_DATE - c.FirstVisit as DAYS_from_FirstEncounter_Date2,'|' as Pipe11,
+	b.PX,'|' as Pipe12,
+	b.PPX,'|' as Pipe13,
+	b.PX_TYPE ,'|' as Pipe14,
+	b.PX_SOURCE,'ENDALONAEND' as lineEND
+	into #NextD_PROCEDURES_FINAL
+from /* provide name of table 1 here: */ #FinalTable1 c 
+join /* provide name of PCORNET table PROCEDURES here: */ [dbo].[PROCEDURES] b on c.PATID=b.PATID 
+join /* provide name of PCORNET table DEMOGRAPPHIC here: */ [dbo].DEMOGRAPHIC d on c.PATID=d.PATID
+where convert(numeric(18,6),(b.ADMIT_DATE - d.BIRTH_DATE))/365.25 between @LowerAge and @UpperAge 
+	and b.ADMIT_DATE between @LowerTimeFrame and @UpperTimeFrame;
 ---------------------------------------------------------------------------------------------------------------
